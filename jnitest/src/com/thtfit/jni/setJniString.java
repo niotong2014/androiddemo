663e58e3f993c31e530c88a1ce70c3a70557af1a
regan <regan@thtfit.com>
2016-03-08 11:16:34 +0800

    rm jnitest

--- jnitest/src/com/thtfit/jni/setJniString.java
+++
@@ -1,5 +0,0 @@
-package com.thtfit.jni;
-
-public class setJniString {
-
-}
--- jnitest/src/getJniString.cpp
+++
@@ -1,6 +0,0 @@
-#include "com_niotong_jnitest_getJniString.h"
-JNIEXPORT jstring JNICALL Java_com_niotong_jnitest_getJniString_getStringCPP(JNIEnv* env,jclass){
-}
-
-JNIEXPORT jstring JNICALL Java_com_niotong_jnitest_getJniString_getStringJava(JNIEnv * env,jclass){
-}
